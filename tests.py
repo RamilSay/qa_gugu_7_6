74a3e96cf258f904aa677d3a472d58900d942199
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-09-22 22:36:16 +0700

    working

--- tests.py
+++ tests.py
@@ -84,7 +84,7 @@ def test_find_suitable_user():
 
 def test_readable_function():
     func_name = func.__name__.replace('_', ' ').title()
-
+    arg_name = ", ".join([*args])
     open_browser(browser_name="Chrome")
     go_to_companyname_homepage(page_url="https://companyname.com")
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
